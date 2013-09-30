9c2cbea04453b70713a46199e035df0f6641c391
Vitaly Domnikov <dotcypress@gmail.com>
2013-09-30 09:13:08 +0300

    update user state service

--- Vermeil/Notify/ProgressIndicatorService.cs
+++ Vermeil/Notify/ProgressIndicatorService.cs
@@ -12,132 +12,141 @@ using Vermeil.Core;
 
 namespace Vermeil.Notify
 {
-	internal class ProgressIndicatorService : IProgressIndicatorService
-	{
-		private readonly List<TaskHolder> _holders = new List<TaskHolder>();
-		private readonly object _syncRoot = new object();
+    internal class ProgressIndicatorService : DependencyObject, IProgressIndicatorService
+    {
+        private readonly List<TaskHolder> _holders = new List<TaskHolder>();
+        private readonly object _syncRoot = new object();
 
-		#region Public members
+        #region Public members
 
-		public void ShowIndeterminate(string message, string token)
-		{
-			if (Deployment.Current.Dispatcher.CheckAccess())
-			{
-				UpdateHolder(-1, message, token);
-			}
-			else
-			{
-				Deployment.Current.Dispatcher.BeginInvoke(() => UpdateHolder(-1, message, token));
-			}
-		}
+        public static readonly DependencyProperty IsBusyProperty =
+            DependencyProperty.Register("IsBusy", typeof (bool), typeof (ProgressIndicatorService), new PropertyMetadata(default(bool)));
 
-		public void ShowProgress(double progress, string message, string token)
-		{
-			if (Deployment.Current.Dispatcher.CheckAccess())
-			{
-				UpdateHolder(progress, message, token);
-			}
-			else
-			{
-				Deployment.Current.Dispatcher.BeginInvoke(() => UpdateHolder(progress, message, token));
-			}
-		}
+        public bool IsBusy
+        {
+            get { return (bool) GetValue(IsBusyProperty); }
+            private set { SetValue(IsBusyProperty, value); }
+        }
 
-		public void Hide(string token)
-		{
-			if (Deployment.Current.Dispatcher.CheckAccess())
-			{
-				HideInternal(token);
-			}
-			else
-			{
-				Deployment.Current.Dispatcher.BeginInvoke(() => HideInternal(token));
-			}
-		}
+        public void ShowIndeterminate(string message, string token)
+        {
+            if (Deployment.Current.Dispatcher.CheckAccess())
+            {
+                UpdateHolder(-1, message, token);
+            }
+            else
+            {
+                Deployment.Current.Dispatcher.BeginInvoke(() => UpdateHolder(-1, message, token));
+            }
+        }
 
-		#endregion
+        public void ShowProgress(double progress, string message, string token)
+        {
+            if (Deployment.Current.Dispatcher.CheckAccess())
+            {
+                UpdateHolder(progress, message, token);
+            }
+            else
+            {
+                Deployment.Current.Dispatcher.BeginInvoke(() => UpdateHolder(progress, message, token));
+            }
+        }
 
-		#region Private members
+        public void Hide(string token)
+        {
+            if (Deployment.Current.Dispatcher.CheckAccess())
+            {
+                HideInternal(token);
+            }
+            else
+            {
+                Deployment.Current.Dispatcher.BeginInvoke(() => HideInternal(token));
+            }
+        }
 
-		private void UpdateHolder(double progress, string message, string token)
-		{
-			lock (_syncRoot)
-			{
-				var existing = _holders.FirstOrDefault(x => x.Token == token);
-				if (existing == null)
-				{
-					existing = new TaskHolder
-					{
-						Token = token,
-						Message = message,
-						Progress = progress
-					};
-				}
-				else
-				{
-					existing.Message = message;
-					existing.Progress = progress;
-					_holders.Remove(existing);
-				}
-				_holders.Add(existing);
-				Update();
-			}
-		}
+        #endregion
 
-		private void HideInternal(string token)
-		{
-			if (token == null)
-			{
-				throw new ArgumentException("Token is null");
-			}
-			lock (_syncRoot)
-			{
-				var existing = _holders.FirstOrDefault(x => x.Token == token);
-				if (existing != null)
-				{
-					_holders.Remove(existing);
-				}
-				Update();
-			}
-		}
+        #region Private members
 
-		internal void Update()
-		{
-			var text = "";
-			var isBusy = false;
-			var progress = -1d;
-			var last = _holders.LastOrDefault();
-			if (last != null)
-			{
-				text = last.Message;
-				progress = last.Progress;
-				isBusy = true;
-			}
+        private void UpdateHolder(double progress, string message, object token)
+        {
+            lock (_syncRoot)
+            {
+                var existing = _holders.FirstOrDefault(x => x.Token == token);
+                if (existing == null)
+                {
+                    existing = new TaskHolder
+                    {
+                        Token = token,
+                        Message = message,
+                        Progress = progress
+                    };
+                }
+                else
+                {
+                    existing.Message = message;
+                    existing.Progress = progress;
+                    _holders.Remove(existing);
+                }
+                _holders.Add(existing);
+                Update();
+            }
+        }
 
-			Application.Current.RootVisual.
-			            With(x => x as PhoneApplicationFrame).
-			            With(x => x.Content as PhoneApplicationPage).
-			            With(x =>
-				            {
-					            if (SystemTray.GetProgressIndicator(x) == null)
-					            {
-						            SystemTray.SetProgressIndicator(x, new ProgressIndicator());
-					            }
-					            return SystemTray.GetProgressIndicator(x);
-				            }).
-			            Do(x => x.IsVisible = isBusy).
-			            Do(x => x.Text = text).
-			            Do(x => x.IsIndeterminate = progress < 0).
-			            Do(x => x.Value = progress);
-		}
+        private void HideInternal(object token)
+        {
+            if (token == null)
+            {
+                throw new ArgumentException("Token is null");
+            }
+            lock (_syncRoot)
+            {
+                var existing = _holders.FirstOrDefault(x => x.Token == token);
+                if (existing != null)
+                {
+                    _holders.Remove(existing);
+                }
+                Update();
+            }
+        }
 
-		#endregion
+        internal void Update()
+        {
+            var text = "";
+            IsBusy = false;
+            var progress = -1d;
+            var last = _holders.LastOrDefault();
+            if (last != null)
+            {
+                text = last.Message;
+                progress = last.Progress;
+                IsBusy = true;
+            }
 
-		private class TaskHolder
-		{
-			public string Token { get; set; }
-			public string Message { get; set; }
-			public double Progress { get; set; }
-		}
-	}
+            Application.Current.RootVisual.
+                        With(x => x as PhoneApplicationFrame).
+                        With(x => x.Content as PhoneApplicationPage).
+                        With(x =>
+                            {
+                                if (SystemTray.GetProgressIndicator(x) == null)
+                                {
+                                    SystemTray.SetProgressIndicator(x, new ProgressIndicator());
+                                }
+                                return SystemTray.GetProgressIndicator(x);
+                            }).
+                        Do(x => x.IsVisible = IsBusy).
+                        Do(x => x.Text = text).
+                        Do(x => x.IsIndeterminate = progress < 0).
+                        Do(x => x.Value = progress);
+        }
+
+        #endregion
+
+        private class TaskHolder
+        {
+            public object Token { get; set; }
+            public string Message { get; set; }
+            public double Progress { get; set; }
+        }
+    }
 }
