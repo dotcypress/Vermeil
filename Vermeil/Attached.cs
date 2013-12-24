1fd928c49e2a2a22e9414439ec2992597b63637e
Vitaly Domnikov <dotcypress@gmail.com>
2013-12-24 11:26:37 +0200

    bug fix

--- Vermeil/Attached.cs
+++ Vermeil/Attached.cs
@@ -480,7 +480,7 @@ namespace Vermeil
                         0,
                         (sender, ea) =>
                             {
-                                if (scroll.VerticalOffset >= scroll.ScrollableHeight)
+                                if (scroll.ScrollableHeight - scroll.VerticalOffset < 0.1)
                                 {
                                     command.Execute(null);
                                 }
